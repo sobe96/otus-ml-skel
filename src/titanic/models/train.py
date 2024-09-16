e999ebd88a1e6c58127c46762f8989ad97c6e4de
Андрей Кондырев <akondyrev@astralinux.ru>
2024-09-16 13:00:59 +0300

    Fix: unused modules in train.py. Finished unnecessary HW
    Executed EDA jup-notebook blocks as a part of my first unnecessary HW at Otus, applied tiny changes to train.py in order to comply wth some error notifications from pylint.

--- src/titanic/models/train.py
+++ src/titanic/models/train.py
@@ -1,13 +1,10 @@
-from typing import NoReturn
 
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.linear_model import LogisticRegression
 
-from ..data.validation import train_test_split
 from ..features.fill import embarked_imputer
-from ..features.extract import is_alone
 from ..features.ordering import ColumnOrderer
 
 __all__ = ["make_baseline_model"]
